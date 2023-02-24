0b025ccf8d8ab39ef7e9f973467eafe6dabc9712
sandip-77 <itssandip1@gmail.com>
2023-02-24 20:02:02 +0530

    changes

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,11 @@
 // craco.config.js
 module.exports = {
-    babel: {
-      plugins: ["babel-plugin-macros"],
+  babel: {
+    plugins: ['babel-plugin-macros'],
+  },
+  style: {
+    postcssOptions: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
     },
-    style: {
-        postcssOptions: {
-        plugins: [require("tailwindcss"), require("autoprefixer")],
-      },
-    },
-  };
+  },
+};
